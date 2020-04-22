815576bcac5cd71271bf271dffec6e15be7bdf8e
Jorge Martinez <36254865+jorgeme0996@users.noreply.github.com>
2020-04-22 09:18:05 -0500

    Fix PORT

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.get('/', (req, res)=> {
     })
 })
 
-app.listen(process.env.PORT, ()=>console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT,process.env.IP || 5000, ()=>console.log('Server running'));
