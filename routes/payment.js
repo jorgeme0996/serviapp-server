4076a48a7759c83fee09337d3cf965ff56dc6843
Jorge Martinez <36254865+jorgeme0996@users.noreply.github.com>
2020-04-27 10:15:18 -0500

    Fix Stripe dep

--- routes/payment.js
+++ routes/payment.js
@@ -2,20 +2,7 @@ require('../config/config');
 
 const express = require('express');
 const {verificarToken, verificaAdmin, verficaGod} = require('../middlewares/auth');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const router  = express.Router();
 
-router.post('/create-payment-intent', verificarToken, async(req, res) => {
-    const intent = await stripe.paymentIntents.create({
-        amount: req.body.amount,
-        currency: req.body.currency.toLowerCase(),
-        // Verify your integration in this guide by including this parameter
-        metadata: {integration_check: 'accept_a_payment'}
-    });
-
-    res.json({
-        client_secret: intent.client_secret
-    })
-})
 
 module.exports = router;
